d9fc1d69e7ac9b3ccc2a752f4fe06861a39b8396
mohameddelsayedd <121309247+mohameddelsayedd@users.noreply.github.com>
2022-12-23 19:10:06 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace disceret
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" start value =  ");
+
+            int start = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(" end value =  ");
+
+            int end = int.Parse(Console.ReadLine());
+
+
+
+            for (int i = start; i <= end; i++)
+            {
+                int x = 0;
+
+                for (int j = 2; j <= i / 2; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        x++;
+                        break;
+                    }
+
+
+                }
+                if (x == 0 && i != 1)
+                    Console.WriteLine("{0}", i);
+            }
+        }
+    }
+}
